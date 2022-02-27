1ac19940d8a9efde4ee75645c8f47677c3a3fe2e
Arnabh Chakravarty <arnab@codrite.com>
2022-02-27 12:29:59 -0500

    Conditional Branching and Publish to new topic

--- src/main/scala/StreamSubPub.scala
+++ src/main/scala/StreamSubPub.scala
@@ -1,5 +1,5 @@
 import org.apache.kafka.common.serialization.Serdes
-import org.apache.kafka.streams.kstream.Produced
+import org.apache.kafka.streams.kstream.{Branched, Named, Predicate, Produced}
 import org.apache.kafka.streams.{KafkaStreams, KeyValue, StreamsBuilder, StreamsConfig}
 
 import java.time.Duration
@@ -21,7 +21,7 @@ object StreamSubPub {
   def propDuplicate: Properties = {
     val streamConfig = new Properties()
 
-    streamConfig.put(StreamsConfig.APPLICATION_ID_CONFIG, "Stream-2")
+    streamConfig.put(StreamsConfig.APPLICATION_ID_CONFIG, "" + System.currentTimeMillis())
     streamConfig.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
     streamConfig.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, classOf[Serdes.StringSerde])
     streamConfig.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, classOf[Serdes.IntegerSerde])
@@ -29,15 +29,29 @@ object StreamSubPub {
     streamConfig
   }
 
-  def main(args: Array[String]): Int = {
+  def main(args: Array[String]): Unit = {
     nameStreamProcessing()
-    nameLengthStreamProcessing()
-    0
+    nameLengthStreamProcessing("duplicateNames")
+    nameLengthStreamProcessing("WhenMoreThan5")
+    nameLengthStreamProcessing("WhenLessThanOrEqualTo5")
   }
 
   def nameStreamProcessing(): Unit = {
     val streamBuilder: StreamsBuilder = new StreamsBuilder
     val streamHandler = streamBuilder.stream[String, String]("names")
+
+    val greaterThan5 : Predicate[String, String] = (x: String, y: String) => (x.length > 5)
+    val LessThanOrEqualTo5 : Predicate[String, String] = (x: String, y: String) => (x.length <= 5)
+
+    val branchedGreaterThan5 = streamHandler.split(Named.as("When"))
+                                            .branch(greaterThan5, Branched.as("MoreThan5"))
+                                            .branch(LessThanOrEqualTo5, Branched.as("LessThanOrEqualTo5"))
+                                            .noDefaultBranch()
+                                                                  //.branch((k, v) => v.length < 5, Branched.as("lessThan5"))
+    println("----------")
+    branchedGreaterThan5.entrySet().forEach(es => println(s"${es.getValue.foreach((k,v) => println(s"${es.getKey} = $v"))}"))
+    branchedGreaterThan5.entrySet().forEach(es => es.getValue.map((k, v) => new KeyValue[String, Integer](k, v.length)).to(es.getKey, Produced.`with`(Serdes.String(), Serdes.Integer())))
+    println("----------")
 
     streamHandler.map((key, value) => new KeyValue[String, Integer](key, value.length)).to("duplicateNames", Produced.`with`(Serdes.String(), Serdes.Integer()))
 
@@ -52,11 +66,11 @@ object StreamSubPub {
     )
   }
 
-  def nameLengthStreamProcessing() : Unit = {
+  def nameLengthStreamProcessing(topicName: String) : Unit = {
     val streamBuilder: StreamsBuilder = new StreamsBuilder
-    val streamHandler = streamBuilder.stream[String, Integer]("duplicateNames")
+    val streamHandler = streamBuilder.stream[String, Integer](topicName)
 
-    streamHandler.foreach((key, value) => println(s"$key --- $value"))
+    streamHandler.foreach((key, value) => println(s"$topicName > $key --- $value"))
 
     val nameStreams = new KafkaStreams(streamBuilder.build(), propDuplicate)
     nameStreams.start()
